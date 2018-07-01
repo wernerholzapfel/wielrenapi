1ae83ca614dbbe06eb36b454ac8272b0539c0338
wholzapfel <werner.holzapfel@trivento.nl>
2018-07-01 23:04:12 +0200

    accept headers aangepast

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ admin.initializeApp({
 let allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Pragma, Expires, Cache-Control, Content-Length, X-Requested-With');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
