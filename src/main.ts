51e41c633633b4f69b7b9c3b938f07c876a39a08
Werner Holzapfel <werner.holzapfel@gmail.com>
2022-12-22 17:22:38 +0100

    small fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import * as admin from 'firebase-admin';
+import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
 
 admin.initializeApp({
     credential: admin.credential.cert({
@@ -36,7 +38,37 @@ let allowCrossDomain = function (req, res, next) {
 };
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule,  { bodyParser: false });
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        /**
+         * Reference: https://docs.nestjs.com/techniques/validation#auto-validation
+         */
+        new ValidationPipe({
+            // Make sure that there's no unexpected data
+            whitelist: true,
+            forbidNonWhitelisted: false,
+            forbidUnknownValues: true,
+
+            /**
+             * Detailed error messages since this is 4xx
+             */
+            disableErrorMessages: false,
+
+            validationError: {
+                /**
+                 * WARNING: Avoid exposing the values in the error output (could leak sensitive information)
+                 */
+                value: false,
+            },
+
+            /**
+             * Transform the JSON into a class instance when possible.
+             * Depends on the type of the data on the controllers
+             */
+            transform: true,
+        }),
+    );
     app.use(allowCrossDomain);
 
     await app.listen(process.env.PORT || 3000)
