5b9ececbdc4fd51451d3cda13b0be10737295e52
Werner Holzapfel <werner.holzapfel@gmail.com>
2025-05-05 22:14:43 +0200

    validaties verwijders op lengte voor renner

--- src/rider/create-rider.dto.ts
+++ src/rider/create-rider.dto.ts
@@ -5,15 +5,15 @@ export class CreateRiderDto {
 
     @IsDefined() @IsString() readonly firstName: string;
 
-    @IsDefined() @IsString() @Length(3, 3) readonly firstNameShort: string;
+    @IsDefined() @IsString() readonly firstNameShort: string;
 
-    @IsDefined() @IsString() @Length(2, 3) readonly initials: string;
+    @IsDefined() @IsString() readonly initials: string;
 
     @IsDefined() @IsString() readonly surName: string;
 
     @IsDefined() @IsString() readonly nationality: string;
 
-    @IsDefined() @IsString() @Length(2, 3) readonly surNameShort: string;
+    @IsDefined() @IsString() readonly surNameShort: string;
 
     @IsDefined() @IsDateString() readonly dateOfBirth: Date;
 
